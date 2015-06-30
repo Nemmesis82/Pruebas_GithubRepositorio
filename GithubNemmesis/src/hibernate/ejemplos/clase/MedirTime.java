116b797566e920271bf687f6788c56d5cd0bbc47
Nemmesis82 <ruth.llaneza@gmail.com>
2015-06-30 20:52:18 +0200

    commit 30062015

--- GithubNemmesis/src/hibernate/ejemplos/clase/MedirTime.java
+++ GithubNemmesis/src/hibernate/ejemplos/clase/MedirTime.java
@@ -10,69 +10,88 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class MedirTime extends HttpServlet {
+@SuppressWarnings("unused")
+public class MedirTime extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
-       
-    
-    public MedirTime() {
-       
-    }
+     
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		/***Medir tiempo de ejecución en el servlet de Isi****/
 		
-		long isiInicio = System.currentTimeMillis();
-		System.out.println("El tiempo del servlet Isi Inicial es: " + isiInicio);
+		/**
+		 * Aqui declaro la variable con la que voy a capturar el tiempo antes de ejecutar el servlet 
+		 */
 		
+		long isiInicio = System.currentTimeMillis();
+				
 		request.getRequestDispatcher("ContarElementosXMLServlet?url=http://www.w3schools.com/xml/note.xml").include(request, response);
 		
 		long isiFinal = System.currentTimeMillis();
-		System.out.println("El tiempo de servlet Isi Final es: " + isiFinal);
 		
 		long isiTotal = isiFinal - isiInicio;
-		System.out.println("El tiempo de servlet Isi Final es: " + isiTotal);
-		
+				
 		PrintWriter pw=response.getWriter();
 		
-		//pw.print("El tiempo de servlet Isi Final es:");
-		//pw.println(isiTotal);	
 		
 		/***Medir tiempo de ejecucion en el servlet de Jorge****/
 		
 		long jorgeInicio = System.currentTimeMillis();
-		System.out.println("El tiempo del servlet Jorge Inicial es: " + jorgeInicio);
-		
+				
 		request.getRequestDispatcher("LeerXML?URL=http://www.w3schools.com/xml/note.xml").include(request, response);
-		//request.getRequestDispatcher("http://172.16.1.37:8090/EjercicioClaseXMLRemoto/ContarElementosXMLServlet").include(request, response);
 		
 		long jorgeFinal = System.currentTimeMillis();
-		System.out.println("El tiempo de servlet Jorge Final es: " + jorgeFinal);
-		
+
 		long jorgeTotal = jorgeFinal - jorgeInicio;
-		System.out.println("El tiempo de servlet Jorge Final es: " + jorgeTotal);
-		
+	
 		PrintWriter pw2=response.getWriter();
-		
-		//pw2.print("El tiempo de servlet Jorge Final es:");
-		//pw2.println(jorgeTotal);
-		
+				
 		if (isiTotal>jorgeTotal)
-		{
-			pw.print("El tiempo de servlet Isi es mayor");
-			pw.print(isiTotal);
+		{	
+			pw.println("<html>");
+			pw.println("<title>Ruth Proyect</title>");
+			pw.println("<b><center>");
+			pw.println("<b><FONT FACE='impact' SIZE=10 COLOR='teal'>TIEMPO DE EJECUCION DEL SERVLET</FONT>");
+			//pw.println("<head>TIEMPO DE EJECUCION DEL SERVLET </head>");
+			pw.println("<body>");
+			pw.println("<b><center>");
+			pw.println("<b><BODY BACKGROUND='gris.jpg'>");
+			pw.println("<hr><h1><FONT COLOR='blue'> Isidoro son: "+ isiTotal+ " "+ "milisegundos."+"<hr></FONT></h1>");
+			pw.println("</hr>");
+			pw.println("</html>");
+			pw.println("</body>");
+		
 		}
 		else if (isiTotal<jorgeTotal)
 		{
-			pw2.print("El tiempo de servlet Jorge es mayor");
-			pw2.print(jorgeTotal);
+			pw2.println("<html>");
+			pw2.println("<title>Ruth Proyect</title>");
+			pw2.println("<b><center>");
+			pw2.println("<b><FONT FACE='impact' SIZE=10 COLOR='teal'>TIEMPO DE EJECUCION DEL SERVLET</FONT>");
+			pw2.println("<body>");
+			pw2.println("<b><center>");
+			pw.println("<b><BODY BACKGROUND='gris.jpg'>");
+			pw2.println("<hr><h1><FONT COLOR='purple'> Jorge son: "+ jorgeTotal+ " "+ "milisegundos."+ "<hr></FONT></h1>");
+			pw2.println("<h2>");
+			pw2.println("</hr>");
+			pw2.println("</html>");
+			pw2.println("</body>");
 		}else
 		{
-			
-			pw2.print("Los dos tardan lo mismo");
-			pw.print(isiTotal);
-			pw2.print(jorgeTotal);
+			pw2.println("<html>");
+			pw2.println("<title>Ruth Proyect</title>");
+			pw2.println("<b><center>");
+			pw2.println("<b><FONT FACE='impact' SIZE=10 COLOR='teal'>TIEMPOS DE EJECUCION DE ISIDORO Y JORGE</FONT>");
+			pw2.println("<body>");
+			pw2.println("<b><center>");
+			pw.println("<b><BODY BACKGROUND='gris.jpg'>");
+			pw2.println("<hr><h1><FONT COLOR='blue'> " + " Isidoro son: " + isiTotal+" "+ "milisegundos</h1>");
+			pw2.println("<hr><h1><FONT COLOR='purple'> " + " Jorge son: " + jorgeTotal+ " "+ "milisegundos."+ "<hr></h1>");
+			pw2.println("</hr>");
+			pw2.println("</html>");
+			pw2.println("</body>");
 		}
 			
 	}
